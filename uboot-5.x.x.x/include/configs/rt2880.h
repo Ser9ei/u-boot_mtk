9881fcdb149bf9fe9eb73c72e8300db6f0fe8006
David Bentham <db260179@gmail.com>
2020-12-20 22:43:45 +0000

    Change bootdelay to 3 seconds

--- uboot-5.x.x.x/include/configs/rt2880.h
+++ uboot-5.x.x.x/include/configs/rt2880.h
@@ -98,7 +98,7 @@ extern unsigned int  CFG_BLOCKSIZE;
 #define CONFIG_BAUDRATE		115200	/* 115200 by default */
 #endif
 
-#define CONFIG_BOOTDELAY	1	/* autoboot after 1 seconds	*/
+#define CONFIG_BOOTDELAY	3	/* autoboot after 3 seconds	*/
 
 #define CONFIG_SERVERIP 192.168.1.2
 #define CONFIG_IPADDR 192.168.1.1
