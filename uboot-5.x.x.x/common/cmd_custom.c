940991d2cc29af3322f7276fac7e05fefc8c451e
David Bentham <david.bentham@lvs.co.uk>
2021-04-01 14:54:17 +0100

    Format the startnc and startsc correctly

--- uboot-5.x.x.x/common/cmd_custom.c
+++ uboot-5.x.x.x/common/cmd_custom.c
@@ -20,7 +20,7 @@ int do_start_nc(cmd_tbl_t * cmdtp, int flag, int argc, char *argv[]){
 	return(run_command("setenv stdin nc;setenv stdout nc;setenv stderr nc;version;", 0));
 }
 
-U_BOOT_CMD(startnc, 1, 0, do_start_nc, "start net console\n", NULL);
+U_BOOT_CMD(startnc, 1, 0, do_start_nc, "startnc - net console\n", NULL);
 
 /*
  * Start Serial Console
@@ -29,6 +29,6 @@ int do_start_sc(cmd_tbl_t * cmdtp, int flag, int argc, char *argv[]){
 	return(run_command("setenv stdin serial;setenv stdout serial;setenv stderr serial;version;", 0));
 }
 
-U_BOOT_CMD(startsc, 1, 0, do_start_sc, "start serial console\n", NULL);
+U_BOOT_CMD(startsc, 1, 0, do_start_sc, "startsc - serial console\n", NULL);
 
 #endif /* if defined(CONFIG_NETCONSOLE) */
